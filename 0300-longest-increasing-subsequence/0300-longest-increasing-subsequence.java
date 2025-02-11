403581d82c2f7149e4d12dab9ee6ad75da41c090
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-02-11 19:36:57 +0530

    Time: 6 ms (87.89%), Space: 44.7 MB (33.12%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+
+        for (int n : nums) {
+            if (res.isEmpty() || res.get(res.size() - 1) < n) {
+                res.add(n);
+            } else {
+                int idx = binarysearch(res, n);
+                res.set(idx, n);
+            }
+        }
+        return res.size();
+    }
+
+    private int binarysearch(List<Integer> arr, int target) {
+        int left = 0;
+        int right = arr.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr.get(mid) == target) {
+                return mid;
+            } else if (arr.get(mid) > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
