0f37482ca2573c9b65fc6d02bd0a95cd5665adb7
Daksh Developer group <149083569+DakshAsati@users.noreply.github.com>
2025-03-02 11:55:22 +0530

    Time: 0 ms (100%), Space: 42.5 MB (24.58%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) { // j starts from i+1 to avoid double swap
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) { // Swap elements in each row
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
