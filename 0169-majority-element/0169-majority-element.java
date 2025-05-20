7d5eaa832c842614205d14e17d04991cc54f2943
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-05-20 11:20:09 +0530

    Time: 1 ms (99.85%), Space: 53.1 MB (49.9%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate  = 0;
+
+        int candidate = 0;
         int count = 0;
 
         for(int num : nums){
@@ -8,13 +9,17 @@ class Solution {
                 candidate = num;
             }
 
+            
             if(num == candidate){
                 count++;
             }else{
                 count--;
             }
         }
-        return candidate;
+
+            return candidate;
+
+        }
         
-    }
 }
+    
