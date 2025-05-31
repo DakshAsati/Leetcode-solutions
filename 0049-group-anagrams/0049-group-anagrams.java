b73d3c7aee2ec75a21b48d1d5b70bbbaa5250d3e
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-05-31 20:22:48 +0530

    Time: 6 ms (98.07%), Space: 48.1 MB (36.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,18 +1,20 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
-        Map<String,List<String>> ans = new HashMap<>();
+        Map<String, List<String>> ans = new HashMap<>();
 
-        for(String s: strs){
-            char[] chars = s.toCharArray();
+        for(String str : strs){
+            char[] chars = str.toCharArray();
             Arrays.sort(chars);
             String key = new String(chars);
             if(!ans.containsKey(key)){
-                ans.put(key, new ArrayList<>());
-            }  
-            ans.get(key).add(s);
+                ans.put(key,new ArrayList<>());
+            }
 
-        }
+            ans.get(key).add(str);
+
+
+        } 
         return new ArrayList<>(ans.values());
         
     }
