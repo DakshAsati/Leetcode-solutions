4d662d9a644c8b8c557ee9c5e1b648d634c604c5
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-06-22 14:10:34 +0530

    Time: 0 ms (100%), Space: 41.9 MB (22.4%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,50 +1,16 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if (needle.isEmpty()) return 0;
+        int n = haystack.length();
+        int m = needle.length();
 
-        int[] lps = computeLPSArray(needle);
-        int i = 0; // index for haystack
-        int j = 0; // index for needle
+        if (m == 0) return 0;
 
-        while (i < haystack.length()) {
-            if (haystack.charAt(i) == needle.charAt(j)) {
-                i++;
-                j++;
-                if (j == needle.length()) {
-                    return i - j;
-                }
-            } else {
-                if (j != 0) {
-                    j = lps[j - 1];  // use LPS to skip comparisons
-                } else {
-                    i++;
-                }
+        for (int i = 0; i <= n - m; i++) {
+            if (haystack.substring(i, i + m).equals(needle)) {
+                return i;
             }
         }
 
         return -1;
-    }
-
-    private int[] computeLPSArray(String pattern) {
-        int[] lps = new int[pattern.length()];
-        int length = 0; // length of the previous longest prefix suffix
-        int i = 1;
-
-        while (i < pattern.length()) {
-            if (pattern.charAt(i) == pattern.charAt(length)) {
-                length++;
-                lps[i] = length;
-                i++;
-            } else {
-                if (length != 0) {
-                    length = lps[length - 1];
-                } else {
-                    lps[i] = 0;
-                    i++;
-                }
-            }
-        }
-
-        return lps;
     }
 }
