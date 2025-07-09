1b43f03a8b3d57ec7399acfa374395abe590a65f
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-07-09 23:14:40 +0530

    Time: 0 ms (100%), Space: 40.2 MB (95.47%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,17 +1,16 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        return countPaths(0, 0, m, n, dp);
-    }
-
-    private int countPaths(int sr, int sc, int m, int n, int[][] dp) {
-        if (sr >= m || sc >= n) return 0; // Out of bounds
-        if (sr == m - 1 && sc == n - 1) return 1; // Reached destination
+        return countpaths(0,0,m,n,dp);
         
-        if (dp[sr][sc] != 0) return dp[sr][sc]; // Use precomputed result
+    }
+    private int countpaths(int sr,int sc, int m, int n, int[][]dp){
+        if(sr >= m || sc >= n)return 0;
+        if(sr == m - 1 && sc == n - 1)return 1;
 
-        // Store the result in dp array
-        dp[sr][sc] = countPaths(sr + 1, sc, m, n, dp) + countPaths(sr, sc + 1, m, n, dp);
+        if(dp[sr][sc] != 0) return dp[sr][sc];
+
+        dp[sr][sc] = countpaths(sr + 1, sc, m , n, dp) + countpaths(sr, sc + 1, m ,n ,dp);
         return dp[sr][sc];
     }
 }
