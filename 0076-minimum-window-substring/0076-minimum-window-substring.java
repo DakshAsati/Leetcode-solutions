e2ab40fbe119f0eab9cae7fd6d7534ab80ee84dd
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-07-11 02:40:19 +0530

    Time: 68 ms (19.34%), Space: 45.7 MB (20.95%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,46 +1,47 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) {
-            return "";
+        int windowstart = 0, windowend = 0;
+        int charactercount = t.length();
+        String output = "";
+        Map<Character,Integer> charactercountmap = new HashMap<>();
+        for(int i = 0; i < t.length(); i++){
+            char c = t.charAt(i);
+            charactercountmap.put(c,charactercountmap.getOrDefault(c,0) + 1);
         }
+        while(windowend < s.length()){
+            if(charactercountmap.containsKey(s.charAt(windowend))){
+                if(charactercountmap.get(s.charAt(windowend)) > 0){
+                    charactercount = charactercount - 1;
 
-        Map<Character, Integer> charCount = new HashMap<>();
-        for (char ch : t.toCharArray()) {
-            charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
-        }
+                }
+                charactercountmap.put(s.charAt(windowend), charactercountmap.get(s.charAt(windowend)) - 1);
 
-        int targetCharsRemaining = t.length();
-        int[] minWindow = {0, Integer.MAX_VALUE};
-        int startIndex = 0;
-
-        for (int endIndex = 0; endIndex < s.length(); endIndex++) {
-            char ch = s.charAt(endIndex);
-            if (charCount.containsKey(ch) && charCount.get(ch) > 0) {
-                targetCharsRemaining--;
             }
-            charCount.put(ch, charCount.getOrDefault(ch, 0) - 1);
-
-            if (targetCharsRemaining == 0) {
-                while (true) {
-                    char charAtStart = s.charAt(startIndex);
-                    if (charCount.containsKey(charAtStart) && charCount.get(charAtStart) == 0) {
-                        break;
+            if(charactercount == 0){
+                while(windowstart <= windowend){
+                    if(charactercountmap.containsKey(s.charAt(windowstart))){
+                        boolean isSubArrayStillValid = charactercountmap.get(s.charAt(windowstart)) + 1 <= 0 ? true : false;
+                        if(isSubArrayStillValid){
+                            charactercountmap.put(s.charAt(windowstart), charactercountmap.get(s.charAt(windowstart)) + 1);
+                            windowstart++;
+                        }else{
+                            break;
+                        }
+                    }else{
+                        windowstart++;
                     }
-                    charCount.put(charAtStart, charCount.getOrDefault(charAtStart, 0) + 1);
-                    startIndex++;
                 }
 
-                if (endIndex - startIndex < minWindow[1] - minWindow[0]) {
-                    minWindow[0] = startIndex;
-                    minWindow[1] = endIndex;
+                String temp = s.substring(windowstart, windowend + 1);
+                if(output.isEmpty() || output.length() > temp.length()){
+                    output = temp;
                 }
 
-                charCount.put(s.charAt(startIndex), charCount.getOrDefault(s.charAt(startIndex), 0) + 1);
-                targetCharsRemaining++;
-                startIndex++;
             }
+            windowend++;
         }
+        return output;
 
-        return minWindow[1] >= s.length() ? "" : s.substring(minWindow[0], minWindow[1] + 1);        
+        
     }
 }
