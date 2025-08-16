89187da3906ff323cbe1130233c632c1f8861df4
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-08-16 08:06:01 +0530

    Time: 7 ms (6.65%), Space: 41.3 MB (5.83%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.java
+++ 1448-maximum-69-number/1448-maximum-69-number.java
@@ -1,14 +1,28 @@
 class Solution {
     public int maximum69Number (int num) {
-        char[] arr = String.valueOf(num).toCharArray();
+        String s = Integer.toString(num);
+        if(s == null || s.length() == 0) return 0;  // int return karna hai
 
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] == '6') {  // pehli 6 ko 9 me badal do
-                arr[i] = '9';
-                break; // ek hi change allowed hai
+        int maxvalue = num;  // abhi tak ka max original number hi hai
+
+        for(int i = 0; i < s.length(); i++){
+            // ek-ek character nikaalo
+            String sub = s.substring(i, i+1);
+
+            String changed = s;
+            if(sub.equals("6")){
+                // "6" ko "9" se replace karo
+                changed = s.substring(0, i) + "9" + s.substring(i+1);
+            } else if(sub.equals("9")){
+                // "9" ko "6" se replace karo (agar tumhe dono check karne the)
+                changed = s.substring(0, i) + "6" + s.substring(i+1);
             }
+
+            // string ko integer me convert karo
+            int val = Integer.parseInt(changed);
+            maxvalue = Math.max(maxvalue, val);
         }
 
-        return Integer.parseInt(new String(arr));
+        return maxvalue;
     }
 }
