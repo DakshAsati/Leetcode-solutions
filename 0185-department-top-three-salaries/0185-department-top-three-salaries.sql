490f0a4bbfedf731442932e10d32ac1c19fbc45f
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-08-30 15:25:34 +0530

    Time: 842 ms (92.68%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,11 @@
+SELECT d.name AS Department,
+       e.name AS Employee,
+       e.salary AS Salary
+FROM (
+    SELECT e.*,
+           DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS rank_num
+    FROM Employee e
+) e
+JOIN Department d
+  ON e.departmentId = d.id
+WHERE e.rank_num <= 3;
