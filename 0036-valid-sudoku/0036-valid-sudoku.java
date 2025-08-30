06098c1229bdf8e67772bbf599e974c5f10cf8e8
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-08-30 08:04:32 +0530

    Time: 3 ms (52.39%), Space: 44.5 MB (56.61%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,28 +1,39 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] row = new HashSet[9];
+        HashSet<Character>[] cols = new HashSet[9];
+        HashSet<Character>[] box = new HashSet[9];
+
 
         for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                if(!isValid(i,j,board)){
+            row[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            box[i] = new HashSet<>();
+
+        }
+
+        for(int r = 0; r < 9; r++){
+            for(int c = 0; c < 9; c++){
+                char val = board[r][c];
+
+
+                if(val == '.') continue;
+
+
+                int boxindex = (r / 3)* 3 + (c / 3);
+
+
+                if(row[r].contains(val) || cols[c].contains(val) || box[boxindex].contains(val)){
                     return false;
                 }
+
+            
+            row[r].add(val);
+            cols[c].add(val);
+            box[boxindex].add(val);
             }
         }
         
-        return true;
-    }
-
-    private boolean isValid(int row, int col, char[][]board){
-        if(board[row][col] == '.')return true;
-
-
-        for(int i = 0; i < 9; i++){
-            if(col != i && board[row][i] == board[row][col])return false;
-            if(row != i && board[i][col] == board[row][col])return false;
-            if((3*(row / 3) + i / 3) != row && (3*(col / 3) + i% 3 ) != col
-            && board[3*(row / 3) + i / 3][3*(col / 3) + i% 3 ] == board[row][col])return false;
-        }
-
         return true;
     }
 }
