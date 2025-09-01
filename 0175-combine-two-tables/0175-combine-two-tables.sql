b404d48859d0adeb285b4e9a59bb76fe014f22e1
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-09-01 09:45:42 +0530

    Time: 398 ms (63.83%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 
 -- SELECT P.lastName, P.firstName, A.city, A.state
--- FROM PERSON P LEFT JOIN Address A on p.PersonId = A.personId
+-- FROM PERSON P LEFT JOIN ADDRESS A on P.personId = A.personId
 
 
-SELECT P.lastName, P.firstName, A.city, A.state
-FROM PERSON P LEFT JOIN ADDRESS A on P.personId = A.personId
+SELECT P.Lastname, P.firstName, A.city, A.state
+FROM Person P LEFT JOIN Address A on P.personId = A.personId
