a8c81c36f6acdd31f13717f87a3fd40ea0e3f5de
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-09-14 17:38:27 +0530

    Time: 10 ms (100%), Space: 45.7 MB (0%) - LeetHub

---
+++ 3997-maximize-sum-of-at-most-k-distinct-elements/3997-maximize-sum-of-at-most-k-distinct-elements.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] maxKDistinct(int[] nums, int k) {
+
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) set.add(num);
+
+
+        List<Integer> list = new   ArrayList<>(set);
+         Collections.sort(list, Collections.reverseOrder());
+
+         int size = Math.min(k , list.size());
+         int[] res = new int[size];
+         for(int i = 0; i < size; i++){
+            res[i] = list.get(i);
+         }
+         return res;
+
+        
+    }
+}
