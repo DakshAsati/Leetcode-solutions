01aaea29afda8417959a4fcf07c9f8fec152896b
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-09-16 11:44:47 +0530

    Time: 51 ms (24.17%), Space: 73.3 MB (91.64%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,25 +1,25 @@
-import java.util.*;
-
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        if (intervals.length == 0) return 0;
+        if(intervals == null || intervals.length == 0)return 0;
+        
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
 
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        int count = 0;
+        int n = intervals.length;
+
         int[] current = intervals[0];
-        
-        for (int i = 1; i < intervals.length; i++) {
-            if (current[1] > intervals[i][0]) {
+        int count = 0;
+
+        for(int i = 1; i < n; i++){
+            if(current[1] > intervals[i][0]){
                 count++;
-                if (current[1] > intervals[i][1]) {
-                    current = intervals[i];
-                }
-            } else {
+            
+            if(current[1] > intervals[i][1]){
+                current = intervals[i];
+            }
+           } else{
                 current = intervals[i];
             }
         }
-        
         return count;
     }
 }
