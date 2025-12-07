7b7094488294a5fde300c5de5e46eeea8815ffc3
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-12-07 18:21:28 +0530

    Time: 1 ms (89.96%), Space: 45.3 MB (49.99%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,34 +1,36 @@
 class Solution {
-
     public void helper(int[] nums, int index, List<List<Integer>> result){
-      int n = nums.length;
-      if(index == n - 1){
-        List<Integer> I = new ArrayList<>();
-
+        int n = nums.length;
+        if(index == n - 1){
+              List<Integer> I =new ArrayList<>();
         for(int i = 0; i < n; i++){
+          
             I.add(nums[i]);
         }
-        result.add(I);
-        return;
-      }
+            result.add(I);
+            return;
+        }
 
-      for(int i = index; i < n; i++){
-        swap(i, index, nums);
-        helper(nums, index + 1, result);
-        swap(i, index, nums);
-      }
+        for(int i = index; i < n; i++){
+            swap(i,index,nums);
+            helper(nums, index + 1, result);
+            swap(i, index, nums);
+        }
 
     }
-      public void swap(int i, int j, int[] nums){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    
+            public void swap(int i, int j, int[] nums){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        
 
-      }
+    
 
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        helper(nums, 0, result);
+        helper(nums, 0,result);
         return result;
         
     }
