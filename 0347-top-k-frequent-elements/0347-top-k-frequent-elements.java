90b2e43ebdb57828c37a307d1d72d0866312549a
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-12-08 17:05:25 +0530

    Time: 14 ms (64.72%), Space: 47.8 MB (43.85%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -3,23 +3,23 @@ class Solution {
         HashMap<Integer,Integer> counter = new HashMap<>();
         for(int n : nums){
             counter.put(n, counter.getOrDefault(n,0) + 1);
+
         }
+            PriorityQueue<Map.Entry<Integer,Integer>> heap = new PriorityQueue<>((a,b) -> Integer.compare(b.getValue(), a.getValue()));
 
-      PriorityQueue<Map.Entry<Integer, Integer>> heap =
-    new PriorityQueue<>((a, b) -> Integer.compare(b.getValue(), a.getValue()));
+            for(var e : counter.entrySet()){
+                heap.offer(e);
+            }
+            int[] res = new int[k];
+            for(int i = 0; i < k; i++){
+                res[i] = heap.poll().getKey();
+            }
 
-
-
-        for(var e : counter.entrySet()){
-            heap.offer(e);
-        }
-
-        int[] res = new int[k];
-        for(int i = 0; i < k; i++){
-            res[i] = heap.poll().getKey();
-        }
-
-        return res;
+            
         
+        return res;
+        }
     }
-}
+
+        
+    
