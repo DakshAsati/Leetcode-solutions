fbed03e024eae1ef169bbf928912f363d399fd46
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-12-12 23:43:23 +0530

    Time: 2 ms (74.89%), Space: 55.8 MB (16.63%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,18 +4,18 @@ class Solution {
         int count = 0;
         int candidate = 0;
 
+
         for(int n : nums){
             if(count == 0){
                 candidate = n;
             }
-
             if(n == candidate){
                 count++;
             }else{
                 count--;
             }
-        }
 
+        }
         return candidate;
         
     }
