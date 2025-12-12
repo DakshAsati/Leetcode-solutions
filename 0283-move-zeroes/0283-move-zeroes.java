a5249316029c6ec9ad622dcaf04c9841a6facaf7
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2025-12-13 00:47:01 +0530

    Time: 2 ms (86.35%), Space: 47.9 MB (19.07%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        int n = nums.length;
+        
+        for(int i = 0; i < n; i++){
+                if(nums[i] != 0){
+                    nums[index++] = nums[i];
+                }
+
+        }
+                while(index < nums.length){
+                    nums[index++] = 0;
+                
+        }
+        
+    }
+}
