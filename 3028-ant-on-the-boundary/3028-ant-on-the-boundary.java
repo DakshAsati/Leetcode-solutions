7396e7fb423a6a9e5e7111472214db5eeefbda6c
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-01-02 18:55:07 +0530

    Time: 1 ms (16%), Space: 44 MB (27.45%) - LeetHub

---
+++ 3028-ant-on-the-boundary/3028-ant-on-the-boundary.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int returnToBoundaryCount(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        int[] prefix = new int[n];
+
+        for(int i = 0; i < n; i++){
+            if(i == 0){
+                prefix[i] = nums[i];
+            }else{
+                prefix[i] = prefix[i - 1] + nums[i];
+
+                if(prefix[i] == 0){
+                    count++;
+                }
+                
+            }
+    
+        }
+        return count;
+    }
+}
+
+// nums[i] < 0 <- left[-nums[i]]
+// nums[i] > 0 <- right[nums[i]]
+// use prefix sum 
+// for example -> [2,3,-5]
+// prefix result -> [2,5,0]
+// in first step -> nums[i] > 0 so 2 step right to the boundary
+// in second step -> nums[i] > 0 so 5 step right 
+// in third step -> nums[i] == 0 so on boundary[1]
+
+
+// prefix[i] < 0 return  0;
+// make a count
+// make prefix sum -> [2,5,0]
+// take last element and analyze it wheeter that is -ve, +ve or  0
+// -ve return  0;
+// +ve 
+// 0 return 1;
