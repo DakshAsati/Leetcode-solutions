0750a32c7fd1b8fd208a116b6890f8f68c476789
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-01-05 01:11:38 +0530

    Time: 11 ms (5.14%), Space: 46.7 MB (14.31%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.java
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.java
@@ -1,37 +1,17 @@
-class Solution {
-    public int numberOfPoints(List<List<Integer>> nums) {
+class Solution{
+public int numberOfPoints(List<List<Integer>> nums) {
+    nums.sort(Comparator.comparing(list -> list.get(0)));
 
-        if (nums == null || nums.size() == 0) return 0; // 🔴 REQUIRED
+    int current = 0, numberOfPoints = 0;
+    for (List<Integer> car : nums) {
+      if (car.get(0) > current)
+        numberOfPoints += car.get(1) - car.get(0) + 1;
+      else if (car.get(1) > current)
+        numberOfPoints += car.get(1) - current;
 
-        int n = nums.size();
-        int[][] intervals = new int[n][2];
-
-        for (int i = 0; i < n; i++) {
-            intervals[i][0] = nums.get(i).get(0);
-            intervals[i][1] = nums.get(i).get(1);
-        }
-
-        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
-
-        List<int[]> result = new ArrayList<>();
-        int[] newInterval = new int[]{intervals[0][0], intervals[0][1]};
-        result.add(newInterval);
-
-        for (int i = 1; i < n; i++) {
-            int[] interval = intervals[i];
-            if (interval[0] <= newInterval[1]) {
-                newInterval[1] = Math.max(newInterval[1], interval[1]);
-            } else {
-                newInterval = new int[]{interval[0], interval[1]};
-                result.add(newInterval);
-            }
-        }
-
-        int ans = 0;
-        for (int[] in : result) {
-            ans += in[1] - in[0] + 1;
-        }
-
-        return ans;
+      current = Math.max(current, car.get(1));
     }
+
+    return numberOfPoints;
+}
 }
