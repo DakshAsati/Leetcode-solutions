81e990e217a96c98f188732d4b16bfabed0683e4
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-04-06 18:47:40 +0530

    Time: 0 ms (100%), Space: 42 MB (85.6%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,16 +1,26 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        return countpaths(0,0,m,n,dp);
+        int [][] grid = new int[m][n];
+
+       
+
+       return solve(grid, 0 ,0,m,n);
+
         
     }
-    private int countpaths(int sr,int sc, int m, int n, int[][]dp){
-        if(sr >= m || sc >= n)return 0;
-        if(sr == m - 1 && sc == n - 1)return 1;
 
-        if(dp[sr][sc] != 0) return dp[sr][sc];
+    public int solve(int[][] grid, int sr, int sc, int m, int n){
+        if(sr >= m || sc >= n) return 0;
+        if(sr == m - 1 && sc == n - 1) return 1;
 
-        dp[sr][sc] = countpaths(sr + 1, sc, m , n, dp) + countpaths(sr, sc + 1, m ,n ,dp);
-        return dp[sr][sc];
+        if(grid[sr][sc] != 0) return grid[sr][sc];
+
+
+        grid[sr][sc] = solve(grid, sr + 1 ,sc, m , n ) + solve( grid, sr, sc + 1, m ,n );
+
+        return grid[sr][sc];
+
+
+
     }
 }
