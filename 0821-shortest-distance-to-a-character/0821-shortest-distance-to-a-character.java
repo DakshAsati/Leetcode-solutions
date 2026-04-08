274c0e463d8e38f8a06fb5f5609b858e968e9e00
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-04-09 00:28:49 +0530

    Time: 1 ms (99.61%), Space: 44.1 MB (56.95%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.java
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.java
@@ -3,9 +3,10 @@ class Solution {
         int l = s.length();
         int[] result = new int[l];
 
+
         int prev = -10000;
 
-        for(int i = 0; i < l ;i++){
+        for(int i = 0; i <= l - 1; i++){
             if(s.charAt(i) == c){
                 prev = i;
             }
@@ -13,16 +14,20 @@ class Solution {
             result[i] = i - prev;
         }
 
-        prev = 10000;
+
+         prev = 10000;
+
         for(int i = l - 1; i >= 0; i--){
             if(s.charAt(i) == c){
-                prev = i;
+                prev =i;
             }
 
             result[i] = Math.min(result[i], prev - i);
         }
 
         return result;
+
+
 
         
     }
