da7a285a89b2784a3af1dda6612072ef52fea936
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-04-18 19:08:56 +0530

    Time: 1 ms (77.28%), Space: 44.1 MB (12.3%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -13,37 +13,59 @@
  *     }
  * }
  */
-
- import java.util.*;
-
 class Solution {
-   public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+   List<List<Integer>> list = new  ArrayList<>();
+        if(root == null) return list;
+       boolean lefttoright = true;
+       Queue<TreeNode> q = new LinkedList<>();
+       q.add(root);
 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        boolean leftToRight = true;
 
-        while (!q.isEmpty()) {
-            int currSize = q.size();
-            List<Integer> level = new ArrayList<>();
-
-            for (int i = 0; i < currSize; i++) {  
-                TreeNode node = q.poll();
-                if (leftToRight) {
-                    level.add(node.val);
-                } else {
-                    level.add(0, node.val); 
-                }
-
-                if (node.left != null) q.add(node.left);
-                if (node.right != null) q.add(node.right);
-            }
+        while(!q.isEmpty()){
+            int size = q.size();
             
-            result.add(level);
-            leftToRight = !leftToRight; 
+        List<Integer> dq = new ArrayList<>();
+        
+
+        for(int i = 0; i < size; i++){
+            
+    
+            TreeNode temp = q.poll();
+
+            if(lefttoright){
+                dq.add(temp.val);
+             
+            }else{
+                dq.add(0,temp.val);
+            }
+
+            if(temp.left != null) q.add(temp.left);
+            if(temp.right != null) q.add(temp.right);
+
         }
-        return result;
-    }
+            list.add(dq);
+
+
+            lefttoright =  !lefttoright;
+            
+
+  
+
+
+        }
+
+        return list;
+
+        }
+
+        
+        
+
+       
+
+
+
+        
+
 }
