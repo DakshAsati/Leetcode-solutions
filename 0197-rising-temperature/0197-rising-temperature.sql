59a6125abafd83bfac72d3719fcca62f414cfafb
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-06-22 14:59:14 +0530

    Time: 442 ms (77.6%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,11 +1,16 @@
-select id
-FROM(
-    SELECT id,
-        recordDate,
-        temperature,
-        LAG(temperature) OVER(ORDER BY recordDate) as prev_temp,
-        LAG(recordDate) OVER(ORDER BY recordDate) as prev_date
-from Weather
-)x
-WHERE temperature > prev_temp and DATEDIFF(recordDate, prev_date) = 1
+-- find previous dates with help of current date
+-- same with tempreture
+-- compare previous one with todays tempreature 
+-- and make sure that difference between previous date and todays date is one only
 
+select id
+from (
+    select id,
+            recordDate,
+            temperature,
+            LAG(temperature) OVER(ORDER by recordDate) as prev_temp,
+            LAG(recordDate) OVER(ORDER BY recordDate) as prev_date
+from Weather
+
+)x
+where temperature > prev_temp and DATEDIFF(recordDate,  prev_date) = 1
