e4687c44f88d970e5e1f35477d0db220ced21bc2
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-06-25 19:59:55 +0530

    Time: 332 ms (99.83%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -9,14 +9,11 @@
 -- order by quality desc;
 
 
-
-select query_name, 
+select query_name,
 ROUND(AVG(rating / position),2) as quality,
 ROUND(
-    SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)
-    * 100.0
-    / COUNT(*),
-2) AS poor_query_percentage
+    SUM(CASE WHEN rating < 3 then 1 else 0 END) / COUNT(*) * 100
+    ,2
+) AS poor_query_percentage
 from Queries
-group by query_name
-
+group by query_name;
