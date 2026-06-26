bc2bbc3d15f1c9af65ca8f27cb2530e0dae8f11f
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-06-26 11:46:40 +0530

    Time: 639 ms (94.3%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,15 +1,12 @@
-SELECT 
-    s.user_id,
-    ROUND(
-        AVG(
-            CASE
-                WHEN c.action = 'confirmed' THEN 1
-                ELSE 0
-            END
-        ),
-        2
-    ) AS confirmation_rate
-FROM Signups s
-LEFT JOIN Confirmations c
-    ON s.user_id = c.user_id
-GROUP BY s.user_id;
+select s.user_id,
+ROUND(
+    AVG(
+        CASE WHEN c.action = 'confirmed' then 1 else 0 end
+
+    ), 2
+) as confirmation_rate
+
+from Signups as s
+left join Confirmations as c
+on s.user_id = c.user_id
+group by user_id;
