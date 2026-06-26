29066a8a82ddc9f9b8597ed13ace1b41bfb5fa82
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-06-26 10:49:46 +0530

    Time: 399 ms (89.15%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,16 +1,14 @@
--- find previous dates with help of current date
--- same with tempreture
--- compare previous one with todays tempreature 
--- and make sure that difference between previous date and todays date is one only
-
 select id
 from (
     select id,
-            recordDate,
-            temperature,
-            LAG(temperature) OVER(ORDER by recordDate) as prev_temp,
-            LAG(recordDate) OVER(ORDER BY recordDate) as prev_date
-from Weather
-
+    recordDate,
+    temperature,
+    LAG(recordDate) OVER(ORDER BY recordDate) as prev_date,
+    LAG(temperature) OVER(ORDER BY recordDate) as prev_temp
+    from Weather
 )x
-where temperature > prev_temp and DATEDIFF(recordDate,  prev_date) = 1
+where prev_temp <  temperature   and DATEDIFF(recordDate, prev_date) = 1;
+
+
+-- find high  temp with compare to previous temp
+-- previous temp sirf ek din pehle ka
