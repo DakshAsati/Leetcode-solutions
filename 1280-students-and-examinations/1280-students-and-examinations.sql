70612f77d8725ed385752364c64d79416173c979
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-06-26 11:19:42 +0530

    Time: 1011 ms (64.18%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,17 +1,19 @@
-SELECT
-    st.student_id,
-    st.student_name,
-    sub.subject_name,
-    COUNT(ex.subject_name) AS attended_exams
-FROM Students st
-CROSS JOIN Subjects sub
-LEFT JOIN Examinations ex
-    ON st.student_id = ex.student_id
-   AND sub.subject_name = ex.subject_name
-GROUP BY
-    st.student_id,
-    st.student_name,
-    sub.subject_name
-ORDER BY
-    st.student_id,
-    sub.subject_name;
+-- means we have to ensure that every student is linked with every exam
+-- and student_id and subject_name will used as cols in output table
+
+
+select st.student_id,
+st.student_name,
+sb.subject_name,
+COUNT(ex.subject_name) as attended_exams
+
+from Students as st
+CROSS JOIN Subjects as sb
+LEFT JOIN Examinations as ex
+
+on st.student_id = ex.student_id
+and sb.subject_name = ex.subject_name
+
+group by st.student_id, st.student_name,sb.subject_name
+
+order by st.student_id, sb.subject_name;
