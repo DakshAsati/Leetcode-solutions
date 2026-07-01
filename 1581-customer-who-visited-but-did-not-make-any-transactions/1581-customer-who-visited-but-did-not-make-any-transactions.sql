f3d9b18e86cd386daf8cbcaa6c838f46b2e5147d
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-07-01 12:48:47 +0530

    Time: 1465 ms (38.96%), Space: 0B (100%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,7 +1,14 @@
+-- Question - 1581
+-- concept - join + group + is null for transaction + 
+-- pattern -> make customer_id table + count_no_trans with count of visit_id + join the table + declae transaction_id is null + group by customer_id
+-- 
+
+
 select customer_id,
-COUNT(*)  as count_no_trans
+COUNT(*) as count_no_trans
 from Visits as v
-left join Transactions as ts
-on v.visit_id = ts.visit_id
-where transaction_id is NULL
+left join Transactions as t
+on v.visit_id = t.visit_id
+where transaction_id is null
 group by v.customer_id;
+
