ab53175b5fc4fb19491e78355062bfa20d929e1d
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-07-20 08:18:04 +0530

    Time: 435 ms (99.45%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -1,6 +1,6 @@
-SELECT 
+select 
     activity_date as day,
     COUNT(DISTINCT user_id) as active_users
 from Activity
-WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
-GROUP by activity_date;
+where activity_date  between '2019-06-28' and '2019-07-27'
+group by activity_date;
