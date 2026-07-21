6d73724c907b8b6b03bec83f957507a4c51f195b
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-07-21 20:00:36 +0530

    Time: 698 ms (16.24%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -3,4 +3,4 @@ from Customer as c
 left join Product as p
 on c.product_key = p.product_key
 group by customer_id
-having count(DISTINCT c.product_key) = (select count(*) from Product);
+having COUNT(DISTINCT c.product_key) = (select count(*) from Product)
