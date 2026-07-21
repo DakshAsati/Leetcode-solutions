a02f3ebf8a48af549445315356044f6f8031af9d
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-07-21 19:54:53 +0530

    Time: 681 ms (18.32%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,23 +1,6 @@
--- make customer_id
--- use table name Customer and product table
--- and match product_key of both tables
--- group by customer id
-
-
-
 select customer_id
 from Customer as c
 left join Product as p
 on c.product_key = p.product_key
 group by customer_id
-having COUNT(DISTINCT c.product_key) = (SELECT COUNT(*) FROM Product);
-
-
--- select MAX(customer_id) as customer_id
--- from (
--- select customer_id
--- from Customer as c
--- join Product as p
--- on c.product_key = p.product_key
--- group by customer_id
--- ) as id;
+having count(DISTINCT c.product_key) = (select count(*) from Product);
