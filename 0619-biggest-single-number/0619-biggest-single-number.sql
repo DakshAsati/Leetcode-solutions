9a11fc1724f3e4f200f48ebe1b722428c118b448
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-07-21 19:44:45 +0530

    Time: 513 ms (14.94%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,25 +1,8 @@
--- use num cols
--- for apperaing once use HAVING count = 1(for ex)
--- use case that if there is no single number report null
--- for largest single number use MAX
-
--- select num col
--- find number that appears once - 1,4,5,6
--- if there is no single number give null - use CASE
--- from all once numbers find max number using MAX
--- and give in table
-
-
-select MAX(num) as num
-from(
+select max(num) as num
+from (
     select num
     from MyNumbers
     group by num
-    having count(num) = 1
+    having count(*) = 1
 
-) as unique_number;
-
-
-
-
-
+) as unique_number
