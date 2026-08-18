f31da4ebb8798d6e51dbc1c71cd4ff48bf37bb2a
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-08-18 11:35:53 +0530

    Time: 493 ms (13.19%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,13 +1,13 @@
--- select query_name,
--- ROUND(AVG(rating / position),2) as quality,
--- ROUND(SUM(CASE WHEN rating < 3 then 1 or else 0 end) /COUNT(*) * 100, 2) as poor_query_percentage
--- from Queries as q
--- group by query_name
--- order by quality desc, poor_query_percentage asc;
-
 select query_name,
-ROUND(AVG(rating / position), 2) as quality,
-ROUND(SUM(CASE WHEN rating < 3  then 1 else 0 end) / COUNT(*) * 100, 2) as poor_query_percentage
+ROUND(AVG(rating / position),2) as quality,
+ROUND(SUM(CASE WHEN rating < 3 then 1  else 0 end) /COUNT(*) * 100, 2) as poor_query_percentage
 from Queries as q
-group by query_name 
+group by query_name
 order by quality desc, poor_query_percentage asc;
+
+-- select query_name,
+-- ROUND(AVG(rating / position), 2) as quality,
+-- ROUND(SUM(CASE WHEN rating < 3  then 1 else 0 end) / COUNT(*) * 100, 2) as poor_query_percentage
+-- from Queries as q
+-- group by query_name 
+-- order by quality desc, poor_query_percentage asc;
