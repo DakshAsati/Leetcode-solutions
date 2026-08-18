561525554189ca5fd86306081c5cb12af07fd972
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-08-18 11:34:51 +0530

    Time: 413 ms (37.79%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,13 +1,9 @@
--- make table name cols
--- query_name 
--- table_name
--- use WHERE query_name = 'Dog' and 'Cat'
--- (ROUND(rating / percentage) / (SELECT count(*) from query_name), 2) as quality
--- and value  < 3
--- (ROUND(value / select COUNT(*) from rating)) as poor_query_percentage
+-- select query_name,
+-- ROUND(AVG(rating / position),2) as quality,
+-- ROUND(SUM(CASE WHEN rating < 3 then 1 or else 0 end) /COUNT(*) * 100, 2) as poor_query_percentage
+-- from Queries as q
 -- group by query_name
--- order by quality desc;
-
+-- order by quality desc, poor_query_percentage asc;
 
 select query_name,
 ROUND(AVG(rating / position), 2) as quality,
