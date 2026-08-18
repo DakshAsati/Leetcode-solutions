183d4d7b55ffdcf236b43704f22cdde6e5aa9976
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-08-18 12:19:43 +0530

    Time: 335 ms (70.36%), Space: 0B (100%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -1,8 +1,3 @@
--- make class columns
--- count number of each subject
--- make a condition that any subject greater then or equal to 5 
--- group by class
-
 select class
 from Courses
 group by class
