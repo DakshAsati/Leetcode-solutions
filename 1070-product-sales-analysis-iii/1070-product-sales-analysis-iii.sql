2a57113ead0e245aa7c2ccad81e262094307e28f
Daksh Asati <149083569+DakshAsati@users.noreply.github.com>
2026-08-18 12:15:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,7 +1,7 @@
 select product_id,
 year as first_year,quantity,price
-from sales where(product_id, year) in (
+from sales (product_id, year) in (
     select product_id, min(year)
     from sales
     group by product_id
-)
+}
